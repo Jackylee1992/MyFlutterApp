5dd47addea5f8709f96aa4026413dd369039d514
Li Xiangyang <lixiangyang@itouzi.com>
2019-01-08 18:33:43 +0800

    豆瓣电影数据请求、处理、简介列表视图

--- lib/TopMoviePage.dart
+++ lib/TopMoviePage.dart
@@ -1,20 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'MovieItemCell.dart';
 
 class TopMoviePage extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return TopMovieState();
-    }
+  State<StatefulWidget> createState() {
+    return TopMovieState();
+  }
 }
 
 class TopMovieState extends State<TopMoviePage> {
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('豆瓣电影 Top 250'),
-        ),
-        body: Text('data'),
-      );
+  void initState() {
+    super.initState();
+  }
+
+  var _title = '';
+  var _movieItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      body: getBody(_movieItems),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.local_activity),
+        onPressed: () => loadMovieData(),
+      ),
+    );
+  }
+
+  int _apiStartNum = 0;
+
+  loadMovieData() async {
+    try {
+      http
+          .get(
+              'https://api.douban.com/v2/movie/top250?start=$_apiStartNum&count=15')
+          .then((http.Response response) {
+            JsonDecoder decoder = JsonDecoder();
+            final result = decoder.convert(response.body);
+            setState(() {
+              _title = result['title'];
+              _movieItems = result['subjects'];
+              _apiStartNum += 10;
+            });
+          })
+          .catchError((error) => print('error' + error.toString()))
+          .whenComplete(() => print('complete'));
+    } catch (e) {
+      throw Exception(e.toString());
     }
+  }
+
+  String nameArrayString(List list) {
+    List names = [];
+    list.forEach((map) => names.add(map['name']));
+    String result = names.join(' / ');
+    return result;
+  }
+
+  Widget getBody(List items) {
+    List<Widget> itemViewList = [];
+    for (var item in items) {
+      itemViewList.add(MovieItemCell(item: item));
+    }
+    return ListView(
+      children: itemViewList,
+    );
+  }
 }
